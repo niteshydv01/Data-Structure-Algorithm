3d0ddd1de5cc489e43be4c272c6fbdf8249cb9d7
Nitesh Yadav <125591824+niteshydv01@users.noreply.github.com>
2024-03-27 00:21:51 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (94.88%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,31 @@
- #include<vector>
- class Solution {
-private:
-   void solve(vector<int> nums,vector<int> output,int index,vector<vector<int>> & ans) {
-        if(index>=nums.size()){
-            ans.push_back(output);
-            return;
-        }
-        solve(nums,output,index+1,ans);
-        int element=nums[index];
-        output.push_back(element);
-        solve(nums,output,index+1,ans);
+#include <vector>
 
-    }
+using namespace std;
+
+class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> output;
-        int index=0;
-        solve(nums,output,index,ans);
-        return ans;
+        vector<vector<int>> result;
+        vector<int> subset;
+        generateSubsets(nums, 0, subset, result);
+        return result;
+    }
+
+private:
+    void generateSubsets(const vector<int>& nums, int index, vector<int>& subset, vector<vector<int>>& result) {
+        // Add the current subset to the result
+        result.push_back(subset);
+        
+        // Generate subsets starting from the current index
+        for (int i = index; i < nums.size(); ++i) {
+            // Include nums[i] in the current subset
+            subset.push_back(nums[i]);
+            
+            // Recursively generate subsets starting from the next index
+            generateSubsets(nums, i + 1, subset, result);
+            
+            // Exclude nums[i] from the current subset (backtrack)
+            subset.pop_back();
+        }
     }
 };
