a1ce1322f8a96f9d9b503dbd1723d7e4ac6bd053
Nitesh Yadav <125591824+niteshydv01@users.noreply.github.com>
2025-02-08 17:13:39 +0530

    Time: 14 ms (20.82%), Space: 44.8 MB (44.04%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -19,3 +19,30 @@ public:
         return i;
     }
 };
+/*class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int solve(vector<int>& g, vector<int>& s, int i, int j) {
+        if (i >= g.size() || j >= s.size()) return 0; // Base case
+        
+        if (dp[i][j] != -1) return dp[i][j]; // Check memoized value
+
+        if (g[i] <= s[j]) {
+            // Either take the current cookie or skip it
+            return dp[i][j] = max(1 + solve(g, s, i + 1, j + 1), solve(g, s, i, j + 1));
+        } else {
+            // Skip the cookie if it's too small
+            return dp[i][j] = solve(g, s, i, j + 1);
+        }
+    }
+
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        dp.assign(g.size(), vector<int>(s.size(), -1)); // Initialize DP table
+
+        return solve(g, s, 0, 0);
+    }
+};*/
