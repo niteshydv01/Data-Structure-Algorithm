63e78f59143638e9fd7e9cb480decdce1bb71826
Nitesh Yadav <125591824+niteshydv01@users.noreply.github.com>
2025-03-28 01:23:05 +0530

    Time: 0 ms (100%), Space: 16 MB (5.29%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -8,7 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
         if (!head || !head->next) return head; // If list is empty or has one node, return as it is
@@ -31,4 +31,52 @@ public:
 
         return head;
     }
+};*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) return head; // Handle empty or single-node list
+        
+        ListNode* oddHead = new ListNode(0);  // Dummy head for odd list
+        ListNode* evenHead = new ListNode(0); // Dummy head for even list
+        ListNode* odd = oddHead, *even = evenHead;
+        ListNode* curr = head;
+        int index = 1;  // 1-based index tracking
+
+        // Separate odd and even nodes into two lists
+        while (curr) {
+            if (index % 2 != 0) {  // Odd indexed nodes
+                odd->next = new ListNode(curr->val);
+                odd = odd->next;
+            } else {  // Even indexed nodes
+                even->next = new ListNode(curr->val);
+                even = even->next;
+            }
+            curr = curr->next;
+            index++;
+        }
+
+        // Merge odd and even lists
+        odd->next = evenHead->next;  
+
+        ListNode* result = oddHead->next; // Head of the new ordered list
+
+        // Free dummy nodes (to prevent memory leaks)
+        delete oddHead;
+        delete evenHead;
+
+        return result;
+    }
 };
+
