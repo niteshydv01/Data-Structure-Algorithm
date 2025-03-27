9fe86b1d45ed2b0b2ff66b3ddb259d78dd805388
Nitesh Yadav <125591824+niteshydv01@users.noreply.github.com>
2025-03-27 23:50:09 +0530

    Time: 0 ms (100%), Space: 13.7 MB (9.79%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,7 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         ListNode* prev = nullptr;
@@ -29,4 +29,23 @@ public:
         // 'prev' will be the new head of the reversed list
         return prev;
     }
+};*/
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        // Base case: If list is empty or has only one node, return it
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        
+        // Recursive call: Reverse the rest of the list
+        ListNode* newHead = reverseList(head->next);
+
+        // Reverse the current node's pointer
+        head->next->next = head;
+        head->next = nullptr;  // Break the original link
+
+        return newHead;  // Return the new head of the reversed list
+    }
 };
+
