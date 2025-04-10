f47956f3ed401faecb9d70c90e6859ba63f4bcd1
Nitesh Yadav <125591824+niteshydv01@users.noreply.github.com>
2025-04-10 23:57:11 +0530

    Time: 55 ms (51.55%), Space: 61.6 MB (53.21%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
-    // Helper function to check if a substring is a palindrome
-    bool isPalindrome(string& s, int start, int end) {
-        while (start < end) {
-            if (s[start] != s[end]) return false;
-            start++;
-            end--;
-        }
-        return true;
+    vector < vector < string >> partition(string s) {
+      vector < vector < string > > res;
+      vector < string > path;
+      partitionHelper(0, s, path, res);
+      return res;
     }
 
-    // Recursive helper function for backtracking
-    void solve(vector<vector<string>>& ans, vector<string>& temp, string& s, int start) {
-        if (start >= s.size()) {  // If the starting index reaches the end of the string
-            ans.push_back(temp);  // Add the current partition to the answer
-            return;
-        }
-
-        // Explore all possible partitions
-        for (int end = start; end < s.size(); end++) {
-            // Check if the current substring is a palindrome
-            if (isPalindrome(s, start, end)) {
-                temp.push_back(s.substr(start, end - start + 1));  // Add the palindrome substring
-                solve(ans, temp, s, end + 1);  // Recur to partition the remaining substring
-                temp.pop_back();  // Backtrack to try other partitions
-            }
-        }
+  void partitionHelper(int index, string s, vector < string > & path,
+    vector < vector < string > > & res) {
+    if (index == s.size()) {
+      res.push_back(path);
+      return;
     }
-
-    // Main function
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> temp;
-        solve(ans, temp, s, 0);  // Call the recursive function
-        return ans;
+    for (int i = index; i < s.size(); ++i) {
+      if (isPalindrome(s, index, i)) {
+        path.push_back(s.substr(index, i - index + 1));
+        partitionHelper(i + 1, s, path, res);
+        path.pop_back();
+      }
     }
+  }
+
+  bool isPalindrome(string s, int start, int end) {
+    while (start <= end) {
+      if (s[start++] != s[end--])
+        return false;
+    }
+    return true;
+  }
 };
