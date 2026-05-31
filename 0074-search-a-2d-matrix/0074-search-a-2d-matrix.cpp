d1b0f61a69a6892c5a024646dcd901c692ca7312
Nitesh Yadav <125591824+niteshydv01@users.noreply.github.com>
2026-06-01 01:59:36 +0530

    Time: 0 ms (100%), Space: 13.3 MB (44.01%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,40 +1,53 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-          int i,j, a,b, temp, mid_row;
-        //a,b: binary search ifor target row.
-        //i,j: finding target in row.
-        a=0;
-        b=matrix.size()-1;//no. of rows in matrix
-        temp=matrix[0].size(); //no. of columns in matrix.
-        while(a<=b){
-            mid_row=floor((a+b)/2);
-            if(matrix[mid_row][0]<=target && matrix[mid_row][temp-1]>=target){
+
+        int a = 0;
+        int b = matrix.size() - 1;
+        int col = matrix[0].size();
+
+        int midrow = -1;
+
+        // Find candidate row
+        while (a <= b) {
+
+            int mid = a + (b - a) / 2;
+
+            if (matrix[mid][0] <= target &&
+                matrix[mid][col - 1] >= target) {
+
+                midrow = mid;
                 break;
             }
-            else if(target>matrix[mid_row][temp-1]){
-                a=mid_row+1;
+            else if (matrix[mid][0] > target) {
+                b = mid - 1;
             }
-            else if(target<matrix[mid_row][0]){
-                b=mid_row-1;
+            else {
+                a = mid + 1;
             }
         }
-        //now mid_row is the target row to find our target.
-        i=0;
-        j=temp-1;
-        while(i<=j){
-            int mid=floor((i+j)/2);
-            if(matrix[mid_row][mid]==target){
+
+        if (midrow == -1)
+            return false;
+
+        // Binary search inside row
+        int i = 0;
+        int j = col - 1;
+
+        while (i <= j) {
+
+            int mid = i + (j - i) / 2;
+
+            if (matrix[midrow][mid] == target)
                 return true;
-            }
-            else if( target>matrix[mid_row][mid]){
-                i=mid+1;
-            }
-            else if( target<matrix[mid_row][mid]){
-                j=mid-1;
-            }
+
+            else if (matrix[midrow][mid] > target)
+                j = mid - 1;
+
+            else
+                i = mid + 1;
         }
+
         return false;
     }
-        
-    };
+};
