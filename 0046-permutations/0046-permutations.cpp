86bdc686a5db0469f72ab9bccd072d1d95f565e0
Nitesh Yadav <125591824+niteshydv01@users.noreply.github.com>
2026-06-02 14:26:31 +0530

    Time: 2 ms (36.45%), Space: 10.6 MB (69.3%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, vector<vector<int>>& ans, vector<int>& temp, vector<bool>& visited) {
-        
-        if (temp.size() == nums.size()) {
-            ans.push_back(temp);
-            return;
-        }
+   void solve(vector<int>& nums,vector<vector<int>>&ans,vector<int>&curr,vector<bool>&used){
+     if(curr.size()==nums.size()){
+        ans.push_back(curr);
+        return;
+     }
 
-        
-        for (int i = 0; i < nums.size(); i++) {
-            if (!visited[i]) {  
-                visited[i] = true;
-                temp.push_back(nums[i]);
-                
-                
-                solve(nums, ans, temp, visited);
-                
-               
-                temp.pop_back();
-                visited[i] = false;
-            }
+     for(int i=0;i<nums.size();i++){
+        if(!used[i]){
+            used[i]=true;
+            curr.push_back(nums[i]); 
+            solve(nums,ans,curr,used);
+            curr.pop_back();
+            used[i]=false;
         }
-    }
-    
+     }
+   }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        vector<bool> visited(nums.size(), false);  
-        solve(nums, ans, temp, visited);
+
+        vector<vector<int>>ans;
+        vector<int>curr;
+
+        vector<bool>used(nums.size(),false);
+
+        solve(nums,ans,curr,used);
+
         return ans;
+        
     }
 };
