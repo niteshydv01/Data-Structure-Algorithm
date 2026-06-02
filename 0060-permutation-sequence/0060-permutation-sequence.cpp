58533a2240c8f6939d05cc4e7b1b273dbfbae609
Nitesh Yadav <125591824+niteshydv01@users.noreply.github.com>
2026-06-02 14:19:37 +0530

    Time: 1835 ms (5%), Space: 10.2 MB (5.21%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,42 +1,55 @@
 class Solution {
 public:
-    int count = 0; // to keep track of current permutation number
-    string answer; // to store the final k-th permutation
-    bool found = false;
+    int count = 0;
+    string ans;
 
-    void permute(vector<int>& nums, vector<bool>& used, string& current, int k) {
-        if (current.length() == nums.size()) {
+    void permute(vector<int>& element,
+                 vector<bool>& used,
+                 string& curr,
+                 int k) {
+
+        if(!ans.empty()) return;
+
+        if(curr.size() == element.size()) {
             count++;
-            if (count == k) {
-                answer = current;
-                found = true;
+
+            if(count == k) {
+                ans = curr;
             }
             return;
         }
 
-        for (int i = 0; i < nums.size(); ++i) {
-            if (!used[i]) {
+        for(int i = 0; i < element.size(); i++) {
+
+            if(!used[i]) {
+
                 used[i] = true;
-                current += to_string(nums[i]);
-                permute(nums, used, current, k);
-                if (found) return; // early stopping
-                current.pop_back();
+
+                curr += to_string(element[i]);
+
+                permute(element, used, curr, k);
+
+                curr.pop_back();
+
                 used[i] = false;
             }
         }
     }
 
     string getPermutation(int n, int k) {
-        vector<int> nums;
-        for (int i = 1; i <= n; ++i) {
-            nums.push_back(i);
+
+        vector<int> element;
+
+        for(int i = 1; i <= n; i++) {
+            element.push_back(i);
         }
 
         vector<bool> used(n, false);
-        string current = "";
-        permute(nums, used, current, k);
-        return answer;
+
+        string curr = "";
+
+        permute(element, used, curr, k);
+
+        return ans;
     }
-
-
 };
