f991e61be7a8962e411a1ba5591dc3ef2c9b0f6b
Nitesh Yadav <125591824+niteshydv01@users.noreply.github.com>
2026-06-18 19:28:26 +0530

    Time: 23 ms (83.58%), Space: 17.5 MB (43.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,42 +1,42 @@
 class Solution {
-private:
-    void bfs(int row,int col,vector<vector<int>> &vis,vector<vector<char>>& grid){
-        vis[row][col]=1;
-        int n=grid.size();
-        int m=grid[0].size();
-        queue<pair<int,int>> q;
-        q.push({row,col});
-        while(!q.empty()){
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
-            // check if the neighbours has 1
-            int dr[]={1,0,-1,0};
-            int dc[]={0,1,0,-1};
-            for(int i=0;i<4;i++){
-                int nrow=row+dr[i];
-                int ncol=col+dc[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
-                    vis[nrow][ncol]=1;
-                    q.push({nrow,ncol});
-                }
-            }
+public:
+    void dfs(int r,int c,vector<vector<int>>&vis,vector<vector<char>>& grid,int delr[],int delc[]){
+        vis[r][c]=1;
+         int n=grid.size();
+         int m=grid[0].size();
+        for(int i=0;i<4;i++){
+            int row=r+delr[i];
+            int col=c+delc[i];
+
+            if(row >= 0 && row < n &&
+                    col >= 0 && col < m &&
+                    !vis[row][col] &&
+                    grid[row][col] == '1')
+                    {
+                        dfs(row,col,vis,grid,delr,delc);
+}
         }
     }
-public:
     int numIslands(vector<vector<char>>& grid) {
+
+        int ilands=0;
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        int count=0;
-        for(int row=0;row<n;row++){
-            for(int col=0;col<m;col++){
-                if(!vis[row][col] && grid[row][col]=='1'){
-                    count++;
-                    bfs(row,col,vis,grid);
+        vector<vector<int>>vis(n,vector<int>(m,0));
+
+        int delr[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j]&& grid[i][j]=='1'){
+                    dfs(i,j,vis,grid,delr,dcol);
+                    ilands++;
                 }
             }
         }
-        return count;
+
+        return ilands;
+        
     }
 };
