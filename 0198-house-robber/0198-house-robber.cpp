d9e2b8b5bd69f54776a08b19e1ef5330f840a3b8
Nitesh Yadav <125591824+niteshydv01@users.noreply.github.com>
2026-07-10 00:06:17 +0530

    Time: 2 ms (7.5%), Space: 10.2 MB (78.71%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,12 +12,27 @@ public:
         int includeCurrent = nums[i] + solve(nums, i + 2, memo);
         int excludeCurrent = solve(nums, i + 1, memo);
         
-        memo[i] = max(includeCurrent, excludeCurrent);
-        return memo[i];
+       return memo[i] = max(includeCurrent, excludeCurrent);
+        //return memo[i];
     }
     
     int rob(vector<int>& nums) {
-        vector<int> memo(nums.size(), -1);
+        vector<int> memo(105, -1);
         return solve(nums, 0, memo);
     }
+ 
+    /*int solve(vector<int>& nums, int n, int i, int sum) {
+        if (i >= n) {
+            return sum;
+        }
+        
+        return max(solve(nums, n, i + 2, sum + nums[i]), solve(nums, n, i + 1, sum));
+    }
+    
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        return solve(nums, n, 0, 0);
+    }*/
+
+
 };
