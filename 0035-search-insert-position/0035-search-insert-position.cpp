50d5561a164eb95f0eed62267fff059c6edb56ff
Nitesh Yadav <125591824+niteshydv01@users.noreply.github.com>
2026-08-23 20:23:32 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.46%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,6 +3,7 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int l=0;
         int h=nums.size()-1;
+        int ans=nums.size();
 
         while(l<=h){
             int mid = l +(h-l)/2;
@@ -10,12 +11,14 @@ public:
                 return mid;
             }
             if(nums[mid]>=target){
+                ans=mid;
+
                 h=mid-1;
             }else{
                 l=mid+1;
             }
         }
-        return l;
+        return  ans;
         
     }
 };
